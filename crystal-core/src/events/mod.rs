42bae9da90b7dcbec2fdacc97a8da384a949b87b
Victor Quiroz <victorhqc@gmail.com>
2020-08-30 14:07:29 +0200

    fix: reconnect on failure

--- crystal-core/src/events/mod.rs
+++ crystal-core/src/events/mod.rs
@@ -7,16 +7,22 @@ use websocket::{ClientBuilder, Message, OwnedMessage};
 
 pub fn listen(lockfile: &'static Lockfile, rx: Receiver<EventName>) {
   thread::spawn(move || {
-    subscribe_to_client(lockfile);
+    let mut sub = Subscriber {
+      status: SubscriberStatus::Idle,
+      lockfile,
+    };
+
+    sub.connect().unwrap();
 
     loop {
       match rx.recv() {
         Ok(EventName::Start) => {
           debug!("Starting events listener");
-          subscribe_to_client(lockfile);
+          sub.connect().unwrap();
         }
         Ok(EventName::Restart) => {
           debug!("Restarting events listener");
+          sub.connect().unwrap();
         }
         Err(e) => {
           panic!("Event listener broke!: {:?}", e);
@@ -26,69 +32,94 @@ pub fn listen(lockfile: &'static Lockfile, rx: Receiver<EventName>) {
   });
 }
 
-fn subscribe_to_client(lockfile: &'static Lockfile) {
-  debug!("Subscribing to League Client");
-  let lockfile = lockfile.get_details().unwrap();
+struct Subscriber {
+  status: SubscriberStatus,
+  lockfile: &'static Lockfile,
+}
 
-  if lockfile.is_none() {
-    debug!("Lockfile is not ready");
-    return;
-  }
+impl Subscriber {
+  pub fn connect(&mut self) -> Result<(), ()> {
+    if self.status == SubscriberStatus::Connected {
+      debug!("Attempting to connect, but connection already started");
 
-  let l = lockfile.unwrap();
-  let mut builder = TlsConnector::builder();
-  let builder = builder.danger_accept_invalid_certs(true);
-  let connector = builder.build().unwrap();
+      return Ok(());
+    }
 
-  let mut headers = Headers::new();
-  headers.set(Authorization(Basic {
-    username: l.username.clone(),
-    password: Some(l.password.clone()),
-  }));
+    debug!("Subscribing to League Client");
+    let lockfile = self.lockfile.get_details().unwrap();
 
-  let addr = format!("wss://{}:{}/", l.address, l.port);
-  debug!("Connecting to {:?}", addr);
+    if lockfile.is_none() {
+      debug!("Lockfile is not ready");
+      return Ok(());
+    }
 
-  let mut client = ClientBuilder::new(&addr)
-    .unwrap()
-    .add_protocol("wamp")
-    .custom_headers(&headers)
-    .connect_secure(Some(connector))
-    .unwrap();
+    let l = lockfile.unwrap();
+    let mut builder = TlsConnector::builder();
+    let builder = builder.danger_accept_invalid_certs(true);
+    let connector = builder.build().unwrap();
 
-  let message = Message::text("[5,\"OnJsonApiEvent\"]");
-  client.send_message(&message).unwrap();
+    let mut headers = Headers::new();
+    headers.set(Authorization(Basic {
+      username: l.username.clone(),
+      password: Some(l.password.clone()),
+    }));
 
-  for message in client.incoming_messages() {
-    let message = match message {
-      Ok(m) => m,
-      Err(e) => {
-        debug!("Error receiving message: {:?}", e);
-        return;
-      }
-    };
+    let addr = format!("wss://{}:{}/", l.address, l.port);
+    debug!("Connecting to {:?}", addr);
 
-    match message {
-      OwnedMessage::Text(txt) => {
-        debug!("Message: {:?}", txt);
-      }
-      OwnedMessage::Ping(data) => {
-        debug!("Ping: {:?}", data);
-      }
-      OwnedMessage::Pong(data) => {
-        debug!("Pong: {:?}", data);
-      }
-      OwnedMessage::Binary(bin) => {
-        debug!("Binary: {:?}", bin);
-      }
-      OwnedMessage::Close(_) => {
-        debug!("Closed connection");
-        return;
+    let mut client = ClientBuilder::new(&addr)
+      .unwrap()
+      .add_protocol("wamp")
+      .custom_headers(&headers)
+      .connect_secure(Some(connector))
+      .unwrap();
+    self.status = SubscriberStatus::Connected;
+
+    let message = Message::text("[5,\"OnJsonApiEvent\"]");
+    client.send_message(&message).unwrap();
+
+    for message in client.incoming_messages() {
+      let message = match message {
+        Ok(m) => m,
+        Err(e) => {
+          debug!("Error receiving message: {:?}", e);
+          self.status = SubscriberStatus::Errored;
+          return Ok(());
+        }
+      };
+
+      match message {
+        OwnedMessage::Text(txt) => {
+          debug!("Message: {:?}", txt);
+        }
+        OwnedMessage::Ping(data) => {
+          debug!("Ping: {:?}", data);
+        }
+        OwnedMessage::Pong(data) => {
+          debug!("Pong: {:?}", data);
+        }
+        OwnedMessage::Binary(bin) => {
+          debug!("Binary: {:?}", bin);
+        }
+        OwnedMessage::Close(_) => {
+          debug!("Closed connection");
+          self.status = SubscriberStatus::Idle;
+          return Ok(());
+        }
       }
     }
-  }
 
-  debug!("Listener done");
+    debug!("Listener done");
+
+    Ok(())
+  }
+}
+
+#[derive(PartialEq)]
+enum SubscriberStatus {
+  Idle,
+  Connected,
+  Errored,
 }
 
 pub enum EventName {
