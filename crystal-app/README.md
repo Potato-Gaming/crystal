a6c4c959163f360eb17e3b7bdac56cf3e05ebac9
Victor Quiroz <victorhqc@gmail.com>
2020-09-08 20:03:23 +0200

    feat: spawn crystal-core as a child process.
    Spawn crystal-core binary when using `CRYSTAL_CORE_BIN` environment variable. This will be useful when distributing production build.

--- crystal-app/README.md
+++ crystal-app/README.md
@@ -6,7 +6,17 @@ Front End for the Crystal Application
 
 ### Requirements
 
-1. Follow instructions for [Tauri](https://tauri.studio/en/docs/getting-started/intro)
+1.  Follow instructions for [Tauri](https://tauri.studio/en/docs/getting-started/intro)
+
+1.  Make sure crystal-core is compiled for production otherwise you'll need t
+
+    ```
+    cargo build --release --bin=crystal-core
+    ```
+
+1.  To run crystal-core when application starts, setup the environment variable: `CRYSTAL_CORE_BIN`
+    otherwise you'll need to run the binary in another terminal. Which is useful during development
+    process.
 
 To make sure all the dependencies needed are ready run
 
@@ -18,4 +28,14 @@ npm run info
 
 ```bash
 npm start
+```
+
+Windows may need to setup the environment first
+
+```powershell
+ # This is an optional flag, when set it'll try to run the binary.
+ # For development p
+
+ $env:CRYSTAL_CORE_BIN="C:\<PATH>\crystal\target\release\crystal-core.exe"
+ npm start
 ```
--- crystal-app/src-tauri/src/main.rs
+++ crystal-app/src-tauri/src/main.rs
@@ -3,16 +3,22 @@
   windows_subsystem = "windows"
 )]
 
+use dotenv::dotenv;
+use std::env;
+use std::process::Command;
+use std::thread;
+
 mod cmd;
 
 fn main() {
+  dotenv().ok();
+  start_crystal_bin();
+
   tauri::AppBuilder::new()
     .invoke_handler(|_webview, arg| {
       use cmd::Cmd::*;
       match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
-        }
+        Err(e) => Err(e.to_string()),
         Ok(command) => {
           match command {
             // definitions for your custom commands from Cmd here
@@ -27,4 +33,23 @@ fn main() {
     })
     .build()
     .run();
+}
+
+fn start_crystal_bin() {
+  let binary_path = env::var("CRYSTAL_CORE_BIN");
+  let binary_path = match binary_path {
+    Ok(b) => b,
+    Err(_) => {
+      println!("CRYSTAL_CORE_BIN not found, make sure crystal-core is running");
+      return;
+    }
+  };
+
+  thread::spawn(move || {
+    let mut child = Command::new(binary_path)
+      .spawn()
+      .expect("Failed to spawn crystal-core");
+    let ecode = child.wait().expect("Failed to wait on crystal-core");
+    println!("Succeeded: {}", ecode.success());
+  });
 }
