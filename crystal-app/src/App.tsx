254d2a92b90950fd51ca11ada046625574354771
Victor Quiroz <victorhqc@gmail.com>
2020-09-11 10:47:24 +0200

    feat: window size, background color

--- crystal-app/src/App.tsx
+++ crystal-app/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import Home from './views/Home';
 import ChampionSelect from './views/ChampionSelect';
+import './app.css';
 
 function App() {
   return (
-    <div data-testid="app">
+    <div className="app" data-testid="app">
       <Switch>
         <Route exact path="/">
           <Home />
